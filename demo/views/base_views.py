9fa2b435be5c4faa9962ef633ce25f32430580c7
jjeamin <woalsdl600@gmail.com>
2021-09-26 17:27:02 +0900

    Update paginator

--- demo/views/base_views.py
+++ demo/views/base_views.py
@@ -1,9 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from ..models import Demo
 
 def index(request):
+    page = request.GET.get('page', '1')
+
     demo_list = Demo.objects.all()
-    context = {'demo_list': demo_list}
+
+    # page
+    paginator = Paginator(demo_list, 12)
+    page_obj = paginator.get_page(page)
+
+    context = {'demo_list': page_obj}
+
     return render(request, 'demo/demo_list.html', context)
 
 def detail(request, demo_id):
